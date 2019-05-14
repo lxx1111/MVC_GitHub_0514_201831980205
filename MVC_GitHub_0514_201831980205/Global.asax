e8094e6d404505f1a91457df6c59f1fe608c0ff8
1749434456 <1749434456@qq.com>
2019-05-14 17:45:12 +0800

    版本1

---
+++ MVC_GitHub_0514_201831980205/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_GitHub_0514_201831980205.MvcApplication" Language="C#" %>
---
+++ MVC_GitHub_0514_201831980205/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_GitHub_0514_201831980205.Startup))]
+namespace MVC_GitHub_0514_201831980205
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
